e3bb6c064c7be359722d9f245dac2b304d59163c
Olga.Fedorova <Olga.Fedorova@nexign-systems.com>
2019-11-20 18:56:33 +0300

    add aouth2

--- organization-service/src/main/java/ofedorova/organizations/Application.java
+++ organization-service/src/main/java/ofedorova/organizations/Application.java
@@ -3,9 +3,11 @@ package ofedorova.organizations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableResourceServer
 public class Application {
 
   public static void main(String[] args) {
