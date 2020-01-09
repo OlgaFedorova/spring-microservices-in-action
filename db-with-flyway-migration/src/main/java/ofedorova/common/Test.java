90dd86efe456b418eebd8c11a15e150053bfa8ea
OlgaFedorova <msg2olga@gmail.com>
2020-01-09 22:05:36 +0300

    add java based flyway-migration

---
+++ db-with-flyway-migration/src/main/java/ofedorova/common/Test.java
@@ -0,0 +1,8 @@
+package ofedorova.common;
+
+public class Test {
+
+    public void print(){
+        System.out.println("Print Test");
+    }
+}
--- db-with-flyway-migration/src/main/java/ofedorova/flyway/V01_6__Insert_test_data.java
+++ db-with-flyway-migration/src/main/java/ofedorova/flyway/V01_6__Insert_test_data.java
@@ -1,5 +1,6 @@
 package ofedorova.flyway;
 
+import ofedorova.common.Test;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 
@@ -9,6 +10,8 @@ public class V01_6__Insert_test_data extends BaseJavaMigration {
 
     @Override
     public void migrate(Context context) throws Exception {
+        Test test = new Test();
+        test.print();
         try (Statement update = context.getConnection().createStatement()) {
             update.execute("INSERT INTO organization.organizations (organization_id, name, contact_name, contact_email, contact_phone)\n" +
                     "        VALUES ('3be5b9aa-91ed-4266-b83f-f09772e77143', 'Test', 'Test','test@hr.com', '111-111-111');");
